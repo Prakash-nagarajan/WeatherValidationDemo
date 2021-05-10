ee558811ca7951390bddf6f4217a69f171fdae4e
prakash-nagarajan <nagarajanprakash7@gmail.com>
2021-05-10 14:39:08 +0530

    in progress - UI completed

--- DemoWeatherComparsion/src/test/java/com/test/base/Base.java
+++ DemoWeatherComparsion/src/test/java/com/test/base/Base.java
@@ -1,31 +1,39 @@
 package com.test.base;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class Base{
-	
+public class Base extends PageFactory{
+
 	WebDriver driver;
-	
-	@Test
+
+	@BeforeTest
 	public void start(){
 		intentiate();
 		launch();
 	}
-	
+
 	public void intentiate(){
 		driver = Driver.getDriverInstance();
+		init();
 	}
-	
+
 	public void launch(){
 		driver.get("https://ndtv.com");
 		driver.manage().window().maximize();
 	}
+
 	
 	@AfterTest
 	public void close(){
-//		driver.quit();
+		//		driver.quit();
 	}
 
 }
